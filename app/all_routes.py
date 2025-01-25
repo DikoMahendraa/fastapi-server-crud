9b5c9bfcc7c1f8c85a226d1cacce01d40a46298e
diko-mahendra <diko.dev99@gmail.com>
2025-01-26 01:15:55 +0700

    feat: final CRUD swagger for users

--- app/all_routes.py
+++ app/all_routes.py
@@ -1,6 +1,6 @@
 # routes for user
 user_create = "/user/create"
 user_all = "/user/all"
-user_detail = "/user/{_id}"
-user_update = "/user/update/{_id}"
-user_delete = "/user/delete/{_id}"
+user_detail = "/user/{id}"
+user_update = "/user/update/{id}"
+user_delete = "/user/delete/{id}"
--- app/routes/users.py
+++ app/routes/users.py
@@ -9,22 +9,36 @@ user_route = APIRouter()
 @user_route.post(all_routes.user_create)
 def new_user(data: Users):
   data = dict(data)
-  res = db.create_user(data["name"], data["email"], data["role"])
+  response = db.create_user(data["name"], data["email"], data["role"])
 
-  return res
+  return response
 
 # get all users
 @user_route.get(all_routes.user_all)
 def get_all_users():
-  res = db.get_all_users()
+  response = db.get_all_users()
 
-  return res
+  return response
 
 
 # get detail user
 @user_route.get(all_routes.user_detail)
-def get_user_detail(_id: int):
-  res = db.get_detail_user(_id)
+def get_user_detail(id: int):
+  response = db.get_detail_user(id)
 
-  return res
+  return response
+
+# update user
+@user_route.put(all_routes.user_update)
+def update_user(id: int, name: str = None, email: str = None, role: str = None):
+  response = db.update_detail_user(id, name, email, role)
+  return response
+
+# delete user
+@user_route.delete(all_routes.user_delete)
+def delete_user(id: int):
+  response = db.delete_user(id)
+
+  return response
+
 
