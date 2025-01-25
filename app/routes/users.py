930f618e08935a84b966bc5941fe4324ec46bf1d
diko-mahendra <diko.dev99@gmail.com>
2025-01-26 01:03:12 +0700

    feat: register endpoint get all users

--- app/routes/users.py
+++ app/routes/users.py
@@ -12,3 +12,10 @@ def new_user(data: Users):
   res = db.create_user(data["name"], data["email"], data["role"])
 
   return res
+
+# get all users
+@user_route.get(all_routes.user_all)
+def get_all_users():
+  res = db.get_all_users()
+
+  return res
